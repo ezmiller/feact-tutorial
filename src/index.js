676acdda1068bb782f0dfc681bc71c3b85efbcbd
Ethan Miller <ethanzanemiller@gmail.com>
2018-10-21 12:04:10 -0400

    Add support for composite components
    Now the library can deal with a component that is not a native html type, but it can't handle the nesting of such elements.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ class FeactDOMComponent {
   }
 
   mountComponent(container) {
+    console.log("FeactDOMComponent::mountComponent: ", this._currentElement);
     const { type, props, children } = this._currentElement;
 
     const domElement = document.createElement(type);
@@ -18,10 +19,35 @@ class FeactDOMComponent {
   }
 }
 
+class FeactCompositeComponentWrapper {
+  constructor(element) {
+    console.log("FeactCompositeComponentWrapper::element", element);
+    this._currentElement = element;
+  }
+
+  mountComponent(container) {
+    const Component = this._currentElement.type;
+    const componentInstance = new Component(this._currentElement.props);
+    const element = componentInstance.render();
+    const domComponentInstance = new FeactDOMComponent(element);
+    return domComponentInstance.mountComponent(container);
+  }
+}
+
 const Feact = {
+  createClass(spec) {
+    function Constructor(props) {
+      this.props = props;
+    }
+
+    Constructor.prototype.render = spec.render;
+
+    return Constructor;
+  },
+
   createElement(type, props, children) {
     const element = {
-      type,
+      type: type,
       props: props || {}
     };
 
@@ -33,12 +59,25 @@ const Feact = {
   },
 
   render(element, container) {
-    const componentInstance = new FeactDOMComponent(element);
+    const componentInstance =
+      typeof element.type === "function"
+        ? new FeactCompositeComponentWrapper(element)
+        : new FeactDOMComponent(element);
     return componentInstance.mountComponent(container);
   }
 };
 
+const HelloFeact = Feact.createClass({
+  constructor(props) {
+    this.props = props;
+  },
+
+  render() {
+    return Feact.createElement("h1", {}, this.props.message);
+  }
+});
+
 Feact.render(
-  Feact.createElement("h1", {}, "Hello Feact!"),
+  Feact.createElement(HelloFeact, { message: "Hello there Feact!" }),
   document.getElementById("app")
 );
